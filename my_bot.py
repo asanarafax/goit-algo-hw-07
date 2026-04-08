20a14cee0679dfdcd6d3be4074345be4dc4438c7
asanarafax <qwertyperty9999@gmail.com>
2026-04-08 12:11:23 +0300

    New code upload

--- my_bot.py
+++ my_bot.py
@@ -28,8 +28,8 @@ class Phone(Field):
 class Birthday(Field):
     def __init__(self, value):
         try:
-            date_obj = datetime.strptime(value, "%d.%m.%Y").date()
-            super().__init__(date_obj)
+            datetime.strptime(value, "%d.%m.%Y")
+            super().__init__(value)
         except ValueError:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
 
